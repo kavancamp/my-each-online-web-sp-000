f1677cde30830f0df25d8d59d02ad6473dd697cc
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-31 15:32:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+array = []
+def my_each(array) # put argument(s) here
+ i = 0
+  while i < array.length
+    # code here
+    i = i + 1
+  end
 end
