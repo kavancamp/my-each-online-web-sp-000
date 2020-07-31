e02fe3e71fe5067463a81a003bb8cdb4d0a4431f
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-31 15:45:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,26 +1,8 @@
-list = ["ab", "ac", "gh"]
-def my_each(array) # put argument(s) here
- i = 0
-  while i < array.length
-    yield (array[i])
-    # code here
-    i = i + 1
-  end
-end
-
-my_each(list) {|i| return "Hello #{i} "}
-
-# Use the collection method
-# To make the *return-value* the answer you want
-# instead of "=> nil"
-def hello(array)
+def my_each(array)
   i = 0
-  collection = []
   while i < array.length
-    collection << yield(array[i])
+    yield(array[i])
     i += 1
   end
-  collection
+  array
 end
-
-hello(list) {|i| "Hello #{i}"}
