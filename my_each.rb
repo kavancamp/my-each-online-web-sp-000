d5e9261bb62bab60124012f56879649d38d32472
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-31 15:38:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-array = []
+list = ["ab", "ac", "gh"]
 def my_each(array) # put argument(s) here
  i = 0
   while i < array.length
-    yield array[i]
+    yield (array[i])
     # code here
     i = i + 1
   end
