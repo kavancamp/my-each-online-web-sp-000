8871cb28a35704824a8eebd138b02ab43c863172
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-31 15:43:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -13,3 +13,14 @@ my_each(list) {|i| puts "Hello #{i} "}
 # Use the collection method
 # To make the *return-value* the answer you want
 # instead of "=> nil"
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(list) {|i| "Hello #{i} what's up?"}
